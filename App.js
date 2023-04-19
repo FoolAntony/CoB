e95f47f926d0c20374fb62bfd47e14d70227db2a
FoolAntony <mrcrazytony@gmail.com>
2023-04-20 00:15:31 +0200

    Minor fixes and comments were added

--- App.js
+++ App.js
@@ -19,29 +19,49 @@ import {Team} from "./Components/SquadController";
 
 const Stack = createNativeStackNavigator();
 
-let team = [{"CB": 5, "MP": [0, 0, 0], "Name": "Weldron", "RV": 2, "Race": "Human", "Skill": [{"Name":"Hellgate","Value": 4}], "Spells": ["Heal"], "WP": 9, "WS": [{"Type":"Sword","Damage": 2}], "Weapon": ["Sword", "Bow"], "id": 20, "Treasure": [],"Inventory": [], "Effects": []}, {"CB": 3, "MP": [1, 1, 2], "Name": "Almuric", "RV": 2, "Race": "Human", "Skill": [{"Name":"Hellgate","Value": 1}], "Spells": ["Thief", "Oratory"], "WP": 8, "WS": [{"Type":"Sword","Damage": 2}], "Weapon": ["Sword", "Dagger"], "id": 1, "Treasure": [],"Inventory": [], "Effects": []}, {"CB": 4, "MP": [1, 2, 3], "Name": "Sliggoth", "RV": 2, "Race": "Dwarf", "Skill": [{"Name":"Detrap","Value": 1}], "Spells": ["Charm", "Blast", "Explosion"],
+let team = Array(9).fill({})
+
+// This is the test squad I used to preform all needed actions. it can be modified with different data.
+
+let team_test = [{"CB": 5, "MP": [0, 0, 0], "Name": "Weldron", "RV": 2, "Race": "Human", "Skill": [{"Name":"Hellgate","Value": 4}], "Spells": ["Heal"], "WP": 9, "WS": [{"Type":"Sword","Damage": 2}], "Weapon": ["Sword", "Bow"], "id": 20, "Treasure": [],"Inventory": [], "Effects": []}, {"CB": 3, "MP": [1, 1, 2], "Name": "Almuric", "RV": 2, "Race": "Human", "Skill": [{"Name":"Hellgate","Value": 1}], "Spells": ["Thief", "Oratory"], "WP": 8, "WS": [{"Type":"Sword","Damage": 2}], "Weapon": ["Sword", "Dagger"], "id": 1, "Treasure": [],"Inventory": [], "Effects": []}, {"CB": 4, "MP": [1, 2, 3], "Name": "Sliggoth", "RV": 2, "Race": "Dwarf", "Skill": [{"Name":"Detrap","Value": 1}], "Spells": ["Charm", "Blast", "Explosion"],
             "WP": 8, "WS": [{"Type":"Axe","Damage": 1}], "Weapon": ["Axe", "Bow"], "id": 17, "Treasure": [],"Inventory": [], "Effects": []}, {"CB": null, "MP": [1, 1, 1], "Name": "A", "RV": 2, "Race": "Elf", "Skill": [{"Name":"Negotiation","Value": 1}], "Spells": ["Flattery"], "WP": 5, "WS": [{"Type":"Bow","Damage": 1}],
             "Weapon": ["Dagger", "T-Dagger"], "id": 27, "Treasure": [],"Inventory": [], "Effects": []}, {"CB": null, "MP": [0, 0, 0], "Name": "G", "RV": 2, "Race": "Elf", "Skill": [{"Name":"Negotiation","Value": 1}], "Spells": ["Rejuvenate"], "WP": 5, "WS": [{"Type":"Bow","Damage": 1}],
             "Weapon": ["Sword", "T-Dagger"], "id": 28, "Treasure": [],"Inventory": [], "Effects": []}, {"CB": null, "MP": [2, 2, 2], "Name": "H", "RV": 1, "Race": "Human", "Skill": [], "Spells": [], "WP": 7, "WS": [{"Type":"Sword","Damage": 1}],
             "Weapon": ["T-Dagger", "Bow"], "id": 31, "Treasure": [],"Inventory": [{"effect": "Combat Bonus", "type": "Medallion"}, {"effect" : "Heal", "type": "Potion"}], "Effects": []}, {}, {}, {}];
 
+// In order to skip some screens, just quote them with Ctrl+/ shortcut.
+// Don't forget to unquote initial parameters for the following screen and quote them when
+// higher screen in on use.
+
+// If you want to test battle system, you can change battle parameter as following:
+//
+// 1. "normal": Normal fight
+// 2. "wondering": Fight with wondering monsters
+// 3. "medusa": Medusa statue event; Fight with medusa
+// 4. "unknown": X the Unknown statue event; Fight with affected hero
+// WARNING! To have unknown battle event stable, you show insert "Enemy" effect into Effects array
+// 5. "vampire": Cabin event; Fight with a vampire
+
 function StackScreens() {
   return (
         <Stack.Navigator>
-          {/*<Stack.Screen initialParams={{level: 1, squad: team, money: 0, XP: 0}} name={"Squad"} component={Squad}/>*/}
-          {/*<Stack.Screen*/}
-          {/*    initialParams={{level: 1, squad: team, money: 0, XP: 0}}*/}
-          {/*    name={"Board"}*/}
-          {/*    options={{*/}
-          {/*        headerRight:() => (*/}
-          {/*            <Button*/}
-          {/*                title={"Information"}*/}
-          {/*            />*/}
-          {/*        )*/}
-          {/*    }}*/}
-          {/*    component={Board}/>*/}
           <Stack.Screen
-              initialParams={{level: 1, squad: team, money: 0, XP: 0, battle:"normal"}}
+              initialParams={{level: 1, squad: team, money: 0, XP: 0}}
+              name={"Squad"}
+              component={Squad}/>
+          <Stack.Screen
+             /* initialParams={{level: 1, squad: team, money: 0, XP: 0}}*/
+              name={"Board"}
+              options={{
+                  headerRight:() => (
+                      <Button
+                          title={"Squad Hub"}
+                      />
+                  )
+              }}
+              component={Board}/>
+          <Stack.Screen
+              /* initialParams={{level: 1, squad: team, money: 0, XP: 0, battle:"normal"}}*/
               name={"Battle"}
               component={Battlefield}/>
           <Stack.Screen
--- Components/GameplayComponents/Squad.js
+++ Components/GameplayComponents/Squad.js
@@ -46,8 +46,7 @@ export default function Squad({route, navigation}) {
         console.log("Sun:"+ sun);
         console.log("Chosen: "+chosen);
         console.log("SpellNum: " + spellNumber)
-        updateTeam(route.params?.squad)
-  }, [state, team, sun, chosen, route.params?.squad])
+  }, [state, team, sun, chosen])
 
     let upTeam = Team;
 
@@ -72,7 +71,6 @@ export default function Squad({route, navigation}) {
       upTeam[state.context.amount] = getRandomHero(id)
       updateTeam(upTeam)
       SetMember(team[state.context.amount]);
-      CompleteSquad.squad = team;
     }
     else{
       console.log("Thank you!")
@@ -157,8 +155,8 @@ const addNewFollower = () => {
              break;
          case "addHeroSpell":
              if (upTeam[state.context.hasSpells].MP[sun] >= spellNumber) {
-                 if (upTeam[state.context.hasSpells].Spells.some(spell => spell === data) === false) {
-                     upTeam[state.context.hasSpells].Spells.push(JSON.parse(JSON.stringify(getSpell(data))))
+                 if (upTeam[state.context.hasSpells].Spells.includes((spell) => spell === data) === false) {
+                     upTeam[state.context.hasSpells].Spells.push(data)
                      updateTeam(upTeam)
                      setModalOption("nextState")
                      spellNumber++
@@ -370,13 +368,13 @@ const addNewFollower = () => {
                     <Text style={styles.modalText}>{member.Name ? "Name: " + member.Name : "Add new member here to see his info!"}</Text>
                     <Text style={styles.modalText}>{member.Race ? "Race: " + member.Race : undefined}</Text>
                     <Text style={styles.modalText}>{member.WP ? "Wound Points / WP: " + member.WP : undefined}</Text>
-                    <Text style={styles.modalText}>{member.MP ? "Mana Points / MP (Red/Yellow/Blue): " + member.MP : undefined}</Text>
+                    <Text style={styles.modalText}>{member.MP ? "Mana Points / MP (Red/Yellow/Blue): \n" + member.MP : undefined}</Text>
                     <Text style={styles.modalText}>{member.Spells ? "Spells: " + member.Spells : undefined}</Text>
                     <Text style={styles.modalText}>{member.RV ? "Resistance / RV: " + member.RV : undefined}</Text>
                     <Text style={styles.modalText}>{member.CB ? "Combat Bonus : " + member.CB : undefined}</Text>
                     <Text style={styles.modalText}>{member.Weapon ? "Weapons: " + member.Weapon : undefined}</Text>
-                    <Text style={styles.modalText}>{member.WS ? "Weapon Skills: " + "+" + member.WS[1] + " for " + member.WS[0] : undefined}</Text>
-                    <Text style={styles.modalText}>{member.Skill ? "Hero Skills: " + member.Skill.find((skill)=> {return skill.Name + ": +" + skill.Value}) : undefined}</Text>
+                    <Text style={styles.modalText}>{member.WS ? "Weapon Skills: " + member.WS.map((weaponSkill) => {return "{" + weaponSkill.Type + ": +" + weaponSkill.Damage + (weaponSkill.Magic ? ", Magic} " : "} ")}) : undefined}</Text>
+                    <Text style={styles.modalText}>{member.Skill ? "Hero Skills: " + member.Skill.map((skill) => {return "{" + skill.Name + ": +" + skill.Value + "} "}) : undefined}</Text>
                     <TouchableOpacity
                         style={[styles.button, styles.buttonClose]}
                         onPress={() => setModalVisible(false)}
@@ -487,7 +485,7 @@ const addNewFollower = () => {
       case "empty":
       case "addHeroes":
         return (
-            <TouchableOpacity  onPress={teamAddNewMember}>
+            <TouchableOpacity style={{paddingTop:55}} onPress={teamAddNewMember}>
               <View style={styles.textButtonContainer}>
                 <Text style={styles.textButton}>Add New Hero!</Text>
               </View>
@@ -496,7 +494,7 @@ const addNewFollower = () => {
 
      case "addFollowers":
         return (
-            <TouchableOpacity  onPress={addNewFollower}>
+            <TouchableOpacity style={{paddingTop:55}}  onPress={addNewFollower}>
               <View style={styles.textButtonContainer}>
                 <Text style={styles.textButton}>Add New Follower!</Text>
               </View>
@@ -504,7 +502,7 @@ const addNewFollower = () => {
         );
      case "primarySun":
         return (
-            <TouchableOpacity  onPress={addNewFollower}>
+            <TouchableOpacity style={{paddingTop:55}}  onPress={addNewFollower}>
               <View style={styles.textButtonContainer}>
                 <Text style={styles.textButton}>Find your Primary Sun!</Text>
               </View>
@@ -513,7 +511,7 @@ const addNewFollower = () => {
 
      case "addHeroSpell":
         return (
-            <TouchableOpacity  onPress={addNewFollower}>
+            <TouchableOpacity style={{paddingTop:55}}  onPress={addNewFollower}>
               <View style={styles.textButtonContainer}>
                 <Text style={styles.textButton}>Add Spells!</Text>
               </View>
@@ -522,7 +520,7 @@ const addNewFollower = () => {
 
       case "finish":
         return (
-            <TouchableOpacity  onPress={NavigateToBoard}>
+            <TouchableOpacity style={{paddingTop:55}}  onPress={NavigateToBoard}>
               <View style={styles.textButtonContainer}>
                 <Text style={styles.textButton}>Next Stage!</Text>
               </View>
@@ -564,7 +562,6 @@ const addNewFollower = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent:"space-evenly",
   },
   text: {
     textAlign:"center",
@@ -584,13 +581,13 @@ const styles = StyleSheet.create({
     alignSelf:"center",
   },
   list: {
-    alignSelf: "center",
-    height:405,
+    height:455,
     width:360,
-    borderColor:"red",
+    paddingTop:40,
+    alignSelf: "center",
   },
   flatlist: {
-    alignContent:"center"
+      alignContent: "center"
   },
   separator: {
     height: 15,
@@ -600,7 +597,7 @@ const styles = StyleSheet.create({
     paddingBottom:150
   },
   headerTextContainer: {
-    height:25,
+      paddingTop:10
   },
   centeredView: {
     flex: 1,
@@ -646,7 +643,6 @@ const styles = StyleSheet.create({
   textButton: {
     fontSize: 20,
     textAlign: "center",
-    paddingTop: 5,
     color:"white"
   },
   textButtonContainer: {
@@ -654,9 +650,9 @@ const styles = StyleSheet.create({
     borderRadius:4,
     width:200,
     alignSelf:'center',
+    justifyContent: 'center',
     height:40,
     backgroundColor:'#4040a1',
-    marginBottom: 100
   },
   input: {
     height: 40,
